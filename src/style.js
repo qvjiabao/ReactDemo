4882a73f59095c2b5b28147481c0870d22a3500d
Jabo.work <qvjiabao@163.com>
2019-06-28 19:47:28 +0800

    更新结构

---
+++ src/style.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const Globalstyle = createGlobalStyle`
+
+    margin:0;
+    padding:0;
+    font-fmaily:sans-serif;
+    background:green;
+  
+`
