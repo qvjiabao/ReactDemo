7fcfcb479225d6c8583c7a333e34fcc39132eb1e
qvjiabao <qvjiabao@163.com>
2019-07-06 21:57:11 +0800

    添加redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from 'react';
 import { Globalstyle } from './style'
 import Header from './common/header/index';
+import { Provider } from 'react-redux';
+import store from './store/index';
 
 function App() {
   return (
     <Fragment>
       <Globalstyle />
-      <Header />
+      <Provider store={store}>
+        <Header />
+      </Provider>
     </Fragment>
   );
 }
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,57 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button, SearchWrapper } from './style';
+import { connect } from 'react-redux';
 
-class Header extends Component {
+const Header = (props) => {
+    return (
+        <HeaderWrapper>
+            <Logo />
+            <Nav>
+                <NavItem className="left active">首页</NavItem>
+                <NavItem className="left">下载App</NavItem>
+                <NavItem className="right">登录</NavItem>
+                <NavItem className="right"><i className="iconfont">&#xe636;</i></NavItem>
+                <SearchWrapper>
+                    <NavSearch
+                        className={props.focused ? "focused" : ""}
+                        onFocus={props.handlerInputFocus}
+                        onBlur={props.handlerInputBlur}
+                    />
+                    <i className={props.focused ? "focused iconfont" : "iconfont"}
+                        dangerouslySetInnerHTML={{ __html: props.focused ? "&#xe617;" : "&#xe63e;" }}>
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            focused: false
-        }
-        this.handlerInputFocus = this.handlerInputFocus.bind(this);
-        this.handlerInputBlur = this.handlerInputBlur.bind(this);
-    }
+                    </i>
+                </SearchWrapper>
+            </Nav>
+            <Addition>
+                <Button className="writting"><i className="iconfont">&#xe617;</i>写文章</Button>
+                <Button className="reg">注册</Button>
+            </Addition>
+        </HeaderWrapper>
+    )
+}
 
-    render() {
-        return (
-            <HeaderWrapper>
-                <Logo />
-                <Nav>
-                    <NavItem className="left active">首页</NavItem>
-                    <NavItem className="left">下载App</NavItem>
-                    <NavItem className="right">登录</NavItem>
-                    <NavItem className="right"><i className="iconfont">&#xe636;</i></NavItem>
-                    <SearchWrapper>
-                        <NavSearch
-                            className={this.state.focused ? "focused" : ""}
-                            onFocus={this.handlerInputFocus}
-                            onBlur={this.handlerInputBlur}
-                        />
-                        <i className={this.state.focused ? "focused iconfont" : "iconfont"}
-                            dangerouslySetInnerHTML={{ __html: this.state.focused ? "&#xe617;" : "&#xe63e;" }}>
-
-                        </i>
-                    </SearchWrapper>
-                </Nav>
-                <Addition>
-                    <Button className="writting"><i className="iconfont">&#xe617;</i>写文章</Button>
-                    <Button className="reg">注册</Button>
-                </Addition>
-            </HeaderWrapper>
-        )
-    }
-
-    handlerInputFocus() {
-        this.setState({
-            focused: true
-        });
-    }
-
-    handlerInputBlur() {
-        this.setState({
-            focused: false
-        });
+const mapStateToProps = (state) => {
+    return {
+        focused: state.focused
     }
 }
 
-export default Header;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handlerInputFocus() {
+            const action = {
+                type: 'search_focus'
+            }
+            dispatch(action);
+        },
+        handlerInputBlur() {
+            const action = {
+                type: 'search_blur'
+            }
+            dispatch(action);
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
