b19c70cf7440470e55c3d297f998f9a30cbe9b0d
unknown <qvjiabao@163.com>
2019-08-02 18:19:41 +0800

    完善代码

--- src/pages/home/component/Topic.js
+++ src/pages/home/component/Topic.js
@@ -1,44 +1,27 @@
 import React, { Component } from 'react';
 import { TopicWrapper, TopicItem } from '../style';
+import { connect } from 'react-redux';
+
 class Topic extends Component {
     render() {
+        const { list } = this.props;
         return (
             <TopicWrapper>
-                <TopicItem>
-                    <img className="topic-pic" src="https://upload.jianshu.io/users/upload_avatars/1835526/83d24e1a-0a0f-43f6-8a1d-289be6101e73.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                    社会热点
-                 </TopicItem>
-                <TopicItem>
-                    <img className="topic-pic" src="https://upload.jianshu.io/users/upload_avatars/1835526/83d24e1a-0a0f-43f6-8a1d-289be6101e73.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                    社会热点
-                 </TopicItem>
-                <TopicItem>
-                    <img className="topic-pic" src="https://upload.jianshu.io/users/upload_avatars/1835526/83d24e1a-0a0f-43f6-8a1d-289be6101e73.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                    社会热点
-                 </TopicItem>
-                <TopicItem>
-                    <img className="topic-pic" src="https://upload.jianshu.io/users/upload_avatars/1835526/83d24e1a-0a0f-43f6-8a1d-289be6101e73.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                    社会热点
-                 </TopicItem>
-                <TopicItem>
-                    <img className="topic-pic" src="https://upload.jianshu.io/users/upload_avatars/1835526/83d24e1a-0a0f-43f6-8a1d-289be6101e73.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                    社会热点
-                 </TopicItem>
-                <TopicItem>
-                    <img className="topic-pic" src="https://upload.jianshu.io/users/upload_avatars/1835526/83d24e1a-0a0f-43f6-8a1d-289be6101e73.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                    社会热点
-                 </TopicItem>
-                <TopicItem>
-                    <img className="topic-pic" src="https://upload.jianshu.io/users/upload_avatars/1835526/83d24e1a-0a0f-43f6-8a1d-289be6101e73.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                    社会热点
-                 </TopicItem>
-                <TopicItem>
-                    <img className="topic-pic" src="https://upload.jianshu.io/users/upload_avatars/1835526/83d24e1a-0a0f-43f6-8a1d-289be6101e73.png?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp" />
-                    社会热点
-                 </TopicItem>
+                {
+                    list.map((item) => (
+                        <TopicItem key={item.get('id')}>
+                            <img className="topic-pic" src={item.get('imgUrl')} />
+                            {item.get('title')}
+                        </TopicItem>
+                    ))
+                }
             </TopicWrapper>
         )
     }
 }
 
-export default Topic;
+const mapState = (state) => ({
+    list: state.get('home').get('topicList')
+});
+
+export default connect(mapState, null)(Topic);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,8 @@
-import { reducer as headerReducer} from '../common/header/sotre';
+import { reducer as headerReducer } from '../common/header/sotre';
+import { reducer as homeReducer } from '../pages/home/store';
 import { combineReducers } from 'redux-immutable';
 
 export default combineReducers({
-    header: headerReducer
+    header: headerReducer,
+    home: homeReducer
 })
